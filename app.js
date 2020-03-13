2a39fba41f1d161c7822a26a08914b17ca4be236
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-03-12 22:02:48 -0700

    create react element and render elment

---
+++ app.js
@@ -0,0 +1,25 @@
+const title = React.createElement(
+       'h1',
+        {id: 'main-title', title: 'This is a title.'},
+        'My First React Element!'
+    );
+
+const desc = Reac.createElement(
+    'p',
+    null,
+    'I ust learned how to create a React node and render it into the DOM.'
+);
+
+const header = React.createElement(
+    'header',
+    null,
+    title,
+    desc
+);
+    
+ReactDOM.render(
+    header,
+    document.getElementById('root');
+);
+
+console.log('bestest');

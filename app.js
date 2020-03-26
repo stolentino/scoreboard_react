51e8ebe0531171310cc3a40fdb26e1948e98f92d
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-03-25 22:30:26 -0700

    change react element to jsx

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-const title = React.createElement(
-       'h1',
-        {id: 'main-title', title: 'This is a title.'},
-        'My First React Element!'
-    );
+const title = <h1>My First React Element</h1>;
 
-const desc = React.createElement(
-    'p',
-    null,
-    'I ust learned how to create a React node and render it into the DOM.'
-);
+const desc = <p>I just learned to create a React node and render it into the dom.</p>;
 
 const header = React.createElement(
     'header',

94747e577976fc332acb73d286baa0da465b62b1
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-03-25 22:52:39 -0700

    update title, desc, and add myTitleID, name jsx elements

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const title = <h1>My First React Element</h1>;
+const title = 'My First React Element';
+const desc = 'I just learned to create a React node and render it into the dom.';
+const myTitleID = 'main-title';
+const name = 'Sam`s';
 
-const desc = <p>I just learned to create a React node and render it into the dom.</p>;
-
-const header = React.createElement(
-    'header',
-    null,
-    title,
-    desc
+const header = (
+    <header>
+    {/*this is a comment*/}
+        <h1 id={myTitleID}>{name} {title}</h1>
+        <p className="main-desc">{desc}</p>
+    </header>
 );
     
 ReactDOM.render(

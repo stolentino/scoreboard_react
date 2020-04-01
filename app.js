97b1e200b52d4d9595cc777e18f1a8bb0d9ed693
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-03-31 21:42:30 -0700

    create counter, app components and apply composition to components

--- app.js
+++ app.js
@@ -6,12 +6,42 @@ const Header = () => {
         </header>
     );
 }
-      
-    
+
+const Player = () => {
+	return (
+		<div className="player">
+			<span className="player-name">
+				Sam
+			</span>
+
+			<Counter />
+		</div>
+	);
+}
+
+const Counter = () => {
+	return (
+		<div className="counter">
+			<button className="counter-action decrement"> - </button>
+			<span className="counter-score">35</span>
+			<button className="counter-action increment"> + </button>
+		</div>
+	);
+}
+
+const App = () => {
+	return (
+		<div className="scoreboard">
+			<Header />
+			{/* Player list */}
+			<Player />
+		</div>
+	);
+}
+
 ReactDOM.render(
-    <Header />,
+    <App />,
     document.getElementById('root')
 );
-
 
 

714e4b98e17685a681e781e1a2722906257fe389
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-04-04 21:59:18 -0700

    add props for name and score

--- app.js
+++ app.js
@@ -3,28 +3,28 @@ const Header = (props) => {
 	return (
 	  <header>
 		<h1>{ props.title }</h1>
-		<span className="stats">Players: { props.totalPlayers }</span>
+		<span className="stats">Players: { props.totalPlayers } </span>
 	  </header>
 	);
   }
   
-  const Player = () => {
+  const Player = (props) => {
 	return (
 	  <div className="player">
 		<span className="player-name">
-		  SAMMY
+		  {props.name}
 		</span>
   
-		<Counter />
+		<Counter score={props.score} />
 	  </div>
 	);
   }
   
-  const Counter = () => {
+  const Counter = (props) => {
 	return (
 	  <div className="counter">
 		<button className="counter-action decrement"> - </button>
-		<span className="counter-score">35</span>
+		<span className="counter-score">{ props.score }</span>
 		<button className="counter-action increment"> + </button>
 	  </div>
 	);
@@ -39,7 +39,10 @@ const Header = (props) => {
 		/>
   
 		{/* Players list */}
-		<Player />
+		<Player name="Samson" score={50}/>
+		<Player name="Treasure" score={90} />
+		<Player name="Ashley" score={85} />
+		<Player name="James" score={80} />
 	  </div>
 	);
   }

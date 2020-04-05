d56c0f0acd2f4be7c64f54a41a7d61675b31d3f5
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-04-05 11:18:09 -0700

    add key to player compoment

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const players = [
 	{
 		name: "Guil",
-		score: 50
+		score: 50,
+		id: 1
 	  },
 	  {
 		name: "Treasure",
-		score: 85
+		score: 85,
+		id: 2
 	  },
 	  {
 		name: "Ashley",
-		score: 95
+		score: 95,
+		id: 3
 	  },
 	  {
 		name: "James",
-		score: 80
+		score: 80,
+		id: 4
 	  }
 ];
 
@@ -62,6 +66,7 @@ const Header = (props) => {
 			<Player 
 				name={player.name}
 				score={player.score}
+				id={player.id.toString()}
 			/>
 		)}
 		{/*<Player name="Samson" score={50}/>

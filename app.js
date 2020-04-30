144cfdfc679ab812773e48b7ca2d4da848515307
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-04-29 21:49:23 -0700

    add onClick event handler to + button and add update setState

--- app.js
+++ app.js
@@ -48,13 +48,19 @@ const Header = (props) => {
 		state = {
 		  score: 0
 		};
+		
+		incrementScore(){
+			this.setState({
+				score: this.state.score + 1
+			});
+		}
 
 	  render(){
 		return (
 			<div className="counter">
 			  <button className="counter-action decrement"> - </button>
 			  <span className="counter-score">{ this.state.score }</span>
-			  <button className="counter-action increment"> + </button>
+			  <button className="counter-action increment" onClick={this.incrementScore}> + </button>
 			</div>
 		  );
 	  }
